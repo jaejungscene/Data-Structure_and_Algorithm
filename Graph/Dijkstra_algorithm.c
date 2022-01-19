50ea0a182ffe8899baf8c464ffd0e04a79ea90bb
Jaejung <ljj0512@gmail.com>
2022-01-19 21:09:34 +0900

    MODIFY Dijkstra.c, Bellman-Ford algorithm.c

--- Graph/Dijkstra_algorithm.c
+++ Graph/Dijkstra_algorithm.c
@@ -1,4 +1,4 @@
-/////////////////     directed graph   ////////////////////
+/////////////////     undirected graph  &  adjacency list    /////////////////
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -7,26 +7,26 @@
 #include <stdbool.h>
 #define MAX_CITIES 20
 #define MAX_NAME 10
-#define MAX_COST 1000000
+#define INT_MAX 2147483647
 
 typedef struct adjNode* adjLink;
 typedef struct adjNode {
-    int cityID;
-    int cost;
-    adjLink next;
+  int cityID;
+  int cost;
+  adjLink next;
 }adjNode;
 
 typedef struct basisNode {
-    char cityName[MAX_NAME + 1];
-    adjLink next;
+  char cityName[MAX_NAME + 1];
+  adjLink next;
 }basisNode;
 
 typedef struct treeNode* treePointer;
 typedef struct treeNode
 {
-    int cityID;
-    treePointer leftChild;
-    treePointer rightChild;
+  int cityID;
+  treePointer leftChild;
+  treePointer rightChild;
 } treeNode;
 
 //function frame
@@ -36,8 +36,7 @@ treePointer insert(treePointer node, int k);
 void make_BST();
 int search_BST(treePointer node, char* cityName);
 void add_adjList(int cityID_1, int cityID_2, int cost);
-void initialize_adjMatrix();
-void make_adjList_adjMatrix();
+void make_adjacency_list();
 void free_BST(treePointer node);
 void free_list();
 void reset();
@@ -48,92 +47,47 @@ void search_shortest_path();
 int choose(int distance[], bool found[]);
 void Dijkstra(int v, int distance[], int pi[]);
 
-bool it_is_same(int dist[], int dist_temp[]);
-void Bellman_Ford(int start, int distance[], int pi[]);
-
 //global variable
 int cities_num = 0;
 basisNode graph[MAX_CITIES] = { 0 };
 treePointer treeHead = NULL;
-int graph_Matrix[MAX_CITIES][MAX_CITIES] = {0};
 
 int main(void) {
-    bool built_path = false;
-    int selection;
-    printf("도시 간 최소 비용 경로 안내 서비스에 오신 것을 환영합니다.\n");
-    while (1) {
-        printf("-----------------------\n");
-        printf("원하는 기능을 선택하세요\n1:입력 2:검색 3:종료\n");
-        scanf("%d", &selection);
-        if (selection == 1) {
-            reset();
-            input_cities();
-            make_BST();
-            make_adjList_adjMatrix();
-            built_path = true;
-        }
-        else if (selection == 2) {
-            if (built_path == false) {
-                printf("아직 도시 간 도로 구축 데이터베이스가 만들어지지 않았습니다.\n");
-                continue;
-            }
-            search_shortest_path();
-        }
-        else if (selection == 3)
-            break;
-        else {
-            printf("잘못된 선택입니다.\n");
-            continue;
-        }
+  bool built_path = false;
+  int selection;
+  printf("도시 간 최소 비용 경로 안내 서비스에 오신 것을 환영합니다.\n");
+  while (1) {
+    printf("-----------------------\n");
+    printf("원하는 기능을 선택하세요\n1:입력 2:검색 3:종료\n");
+    scanf("%d", &selection);
+    if (selection == 1) {
+      reset();
+      input_cities();
+      make_BST();
+      make_adjacency_list();
+      built_path = true;
     }
-    printf("서비스를 종료합니다\n");
-    return 0;
+    else if (selection == 2) {
+      if (built_path == false) {
+        printf("아직 도시 간 도로 구축 데이터베이스가 만들어지지 않았습니다.\n");
+        continue;
+      }
+      search_shortest_path();
+    }
+    else if (selection == 3)
+      break;
+    else {
+      printf("잘못된 선택입니다.\n");
+      continue;
+    }
+  }
+  printf("서비스를 종료합니다\n");
+  return 0;
 }
 
-////////////////////////////////////   Bellman-Ford  /////////////////////////////////////////
-bool it_is_same(int dist[], int dist_temp[]){
-    bool result = true;
-    for(int i=0; i<cities_num; i++){
-        if(dist[i] != dist_temp[i])
-            result = false;
-        dist[i] = dist_temp[i];
-    }
-    return true;
-}
-
-void Bellman_Ford(int start, int distance[], int pi[]) {
-    int dist_temp[MAX_CITIES] = {0};
-    distance[start] = 0; pi[start] = start;
-    for(int i=0; i<cities_num; i++){ //initialize
-        if(i!=start){
-            distance[i] = graph_Matrix[start][i];
-            if(distance[i] != MAX_COST)
-                pi[i] = start;
-            else
-                pi[i] = -1;
-        }
-    }
-    int min;
-    do{
-        for (int u = 0; u < cities_num; u++){
-            if (u != start){
-                min = distance[u];
-                for (int i = 0; i < cities_num; i++){
-                    if(min > distance[i]+graph_Matrix[i][u]){
-                        min = distance[i]+graph_Matrix[i][u];
-                        pi[u] = i;
-                    }
-                }
-                dist_temp[u] = min;
-            }
-        }
-    } while (!it_is_same(distance, dist_temp));
-}
-////////////////////////////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////   Dijkstra   //////////////////////////////////////////
+////////////////////////////////   Dijkstra   /////////////////////////////////////////
 int choose(int distance[], bool found[]) {
-    int i, min = MAX_COST, minpos = -1;
+    int i, min = INT_MAX, minpos = -1;
     for (i = 0; i < cities_num; i++) {
         if (distance[i] < min && !found[i]) {
             min = distance[i];
@@ -151,7 +105,7 @@ void Dijkstra(int v, int distance[], int pi[]) {
     for (int i = 0; i < cities_num; i++) {
         found[i] = false;
         pi[i] = -1;
-        distance[i] = MAX_COST; //MAX_COST == 2147483647
+        distance[i] = INT_MAX; //INT_MAX == 2147483647
     }
     while (temp) {
         distance[temp->cityID] = temp->cost;
@@ -178,9 +132,7 @@ void Dijkstra(int v, int distance[], int pi[]) {
         }
     }
 }
-//////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////////
 void print_path(int s, int e, int distance[], int pi[]) {
     int stack[MAX_CITIES - 2];
     int top = 0, temp = e;
@@ -189,20 +141,19 @@ void print_path(int s, int e, int distance[], int pi[]) {
         top++;
         temp = pi[temp];
     }
-    printf("Path:%s-", graph[s].cityName);
+    printf("Path: %s-", graph[s].cityName);
     top--;
     while (top >= 0) {
         printf("%s-", graph[stack[top]].cityName);
         top--;
     }
-    printf("%s ", graph[e].cityName);
-    printf("Cost:%d\n", distance[e]);
+    printf("%s : ", graph[e].cityName);
+    printf("Cost: %d\n", distance[e]);
 }
 
 void search_shortest_path() {
     char cityName[MAX_NAME + 1];
-    int Dijkstra_distance[MAX_CITIES], Dijkstra_pi[MAX_CITIES];
-    int Bellman_Ford_distance[MAX_CITIES], Bellman_Ford_pi[MAX_CITIES];
+    int distance[MAX_CITIES], pi[MAX_CITIES];
     int start, destination;
 
     while (1) {
@@ -213,18 +164,7 @@ void search_shortest_path() {
             printf("잘못된 도시 이름입니다\n");
             continue;
         }
-        Dijkstra(start, Dijkstra_distance, Dijkstra_pi);
-        Bellman_Ford(start, Bellman_Ford_distance, Bellman_Ford_pi);
-
-        //// test ////
-        for(int i=0; i<cities_num; i++)
-            printf("%d ", Dijkstra_pi[i]);
-        printf("\n");
-        for(int i=0; i<cities_num; i++)
-            printf("%d ", Bellman_Ford_pi[i]);
-        printf("\n");
-        //////////////
-
+        Dijkstra(start, distance, pi);
         break;
     }
     while (1) {
@@ -236,18 +176,15 @@ void search_shortest_path() {
             printf("잘못된 도시 이름입니다\n");
             continue;
         }
-        if (Bellman_Ford_pi[destination] == -1 || Dijkstra_pi[destination] == -1) {
+        if (pi[destination] == -1) {
             printf("경로가 없습니다\n");
             continue;
         }
-        printf("Dijkstra>>      "); print_path(start, destination, Dijkstra_distance, Dijkstra_pi);
-        printf("Bellman-Ford>>  "); print_path(start, destination, Bellman_Ford_distance, Bellman_Ford_pi);
-        printf("\n");
+        print_path(start, destination, distance, pi);
     }
     printf("**검색을 종료합니다\n");
 }
-//////////////////////////////////////////////////////////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////////////////
 
 
 void input_cities() {
@@ -321,31 +258,25 @@ int search_BST(treePointer node, char* cityName) {
 }
 
 void add_adjList(int cityID_1, int cityID_2, int cost) {
-    adjLink new = (adjLink)malloc(sizeof(*new));
+    adjLink new_1 = (adjLink)malloc(sizeof(*new_1));
+    adjLink new_2 = (adjLink)malloc(sizeof(*new_2));
 
-    new->cityID = cityID_2;
-    new->cost = cost;
-    new->next = graph[cityID_1].next;
-    graph[cityID_1].next = new;
+    new_1->cityID = cityID_1;
+    new_1->cost = cost;
+    new_1->next = graph[cityID_2].next;
+    graph[cityID_2].next = new_1;
+
+    new_2->cityID = cityID_2;
+    new_2->cost = cost;
+    new_2->next = graph[cityID_1].next;
+    graph[cityID_1].next = new_2;
 }
 
-void initialize_adjMatrix(){
-    for(int i=0; i<cities_num; i++){
-        for(int j=0; j<cities_num; j++){
-            if(i!=j)
-                graph_Matrix[i][j] = MAX_COST;
-            else
-                graph_Matrix[i][j] = 0;
-        }
-    }
-}
-
-void make_adjList_adjMatrix() {
+void make_adjacency_list() {
     char relation[(MAX_NAME * 2) + 2] = { 0 };
     int cityID_1, cityID_2;
     bool duplicate; //중복된 정보를 판단하기 위해
     adjLink temp; //중복된 정보를 판단하기 위해
-    initialize_adjMatrix();
 
     printf("도시 간 도로 구축 상황을 입력하세요\n");
     while (1) {
@@ -370,9 +301,7 @@ void make_adjList_adjMatrix() {
             printf("중복된 정보입니다\n");
             continue;
         }
-        int cost = atoi(strtok(NULL, " "));
-        add_adjList(cityID_1, cityID_2, cost); //adjacency List에 추가
-        graph_Matrix[cityID_1][cityID_2] = cost; //adjacency Matrix에 추가
+        add_adjList(cityID_1, cityID_2, atoi(strtok(NULL, " "))); //adjacency List에 추가
     }
     printf("도시 간 도록 구축 데이터베이스를 만들었습니다\n");
 }
@@ -419,23 +348,6 @@ mn-kl-13
 ab-mn-5
 gh-ef-5
 lp-qu-10
-.
-
-
-5
-
-0 1 2 3 4
-
-0-1-6
-0-3-7
-1-2-5
-1-3-8
-1-4--4
-2-1--2
-3-2--3
-3-4-9
-4-0-2
-4-2-7
 .
 
 */
