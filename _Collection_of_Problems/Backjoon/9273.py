c63d178fdfb11ce878c877dcb093a44607584373
Jaejung <ljj0512@gmail.com>
2023-03-02 22:22:44 +0900

    temp

---
+++ _Collection_of_Problems/Backjoon/9273.py
@@ -0,0 +1,9 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+    
+n = int(input())
+times = list(map(int, sys.stdin.readline().split()))
+times.sort(reverse=True)
+times = [times[i]-(n-(i+1)) for i in range(n)]
+print(n+max(times)+1)
