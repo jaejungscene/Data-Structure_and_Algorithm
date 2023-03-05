a8ea210f3eb9d8d8adec20898fe17fd88f231d0f
Jaejung <ljj0512@gmail.com>
2023-03-05 19:18:30 +0900

    9273.py

--- _Collection_of_Problems/Backjoon/9273.py
+++ _Collection_of_Problems/Backjoon/9273.py
@@ -1,9 +1,13 @@
-import sys
-def input():
-    return sys.stdin.readline().rstrip()
-    
-n = int(input())
-times = list(map(int, sys.stdin.readline().split()))
-times.sort(reverse=True)
-times = [times[i]-(n-(i+1)) for i in range(n)]
-print(n+max(times)+1)
+from sys import stdin
+
+for line in stdin:
+    N = int(line.rstrip().split("/")[-1])
+    squareN = N**2
+    cnt = 0
+    for i in range(1, squareN+1):
+        if squareN % i == 0:
+            cnt += 1
+    if cnt%2 == 0:
+        print(cnt//2)
+    else:
+        print(cnt//2 +1)
