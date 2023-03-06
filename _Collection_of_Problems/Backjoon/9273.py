5d2cea1c24af2ec397cecbd9934c880e8d03dc4a
Jaejung <ljj0512@gmail.com>
2023-03-06 20:19:37 +0900

    modify

--- _Collection_of_Problems/Backjoon/9273.py
+++ _Collection_of_Problems/Backjoon/9273.py
@@ -1,13 +1,20 @@
-from sys import stdin
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
 
-for line in stdin:
-    N = int(line.rstrip().split("/")[-1])
+
+cntList = []
+for _ in range(4):
+    N = int(input().split("/")[-1])
     squareN = N**2
     cnt = 0
     for i in range(1, squareN+1):
         if squareN % i == 0:
             cnt += 1
     if cnt%2 == 0:
-        print(cnt//2)
+        cntList.append(cnt//2)
     else:
-        print(cnt//2 +1)
+        cntList.append(cnt//2 + 1)
+
+for elem in cntList:
+    print(elem)
