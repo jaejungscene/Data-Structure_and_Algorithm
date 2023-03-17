29e689b3682d56b238341c49c0830a728a561a18
Jaejung <ljj0512@gmail.com>
2023-03-17 21:58:12 +0900

    modify: KMP_algorithm.c

--- Others/KMP_algorithm.c
+++ Others/KMP_algorithm.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#define MAX_LEN
 
-void fail(char *pat)
+int failure[MAX_LEN];
+
+void createFailFunc(char *pat)
+/********************************************************
+ * DESCRIPTION
+ *     create failure function of 'pat'
+ ********************************************************/
 {
-    int failure[strlen(pat)];
-    int i;
+    int i = 0;
     int n = strlen(pat);
     failure[0] = -1;
-    for (int j = 1; j < n; j++)
-    {
+    for (int j = 1; j < n; j++){
         i = failure[j - 1];
         while (pat[j] != pat[i + 1] && i >= 0)
             i = failure[i];
@@ -19,19 +24,37 @@ void fail(char *pat)
         else
             failure[j] = -1;
     }
+}
 
-    for(int j = 0; j<n; j++){
-        printf("%3c", pat[j]);
+int patternMatch(char* string, char* pattern)
+/********************************************************
+ * DESCRIPTION
+ *     Find the first pattern that matches.
+ *     And returns the first index of the matched pattern in 'string'.
+ ********************************************************/
+{
+    int i=0; int j=0;
+    int lens = strlen(string);
+    int lenp = strlen(pattern);
+    while(i<lens && j<lenp){
+        if(string[i] == pattern[j]){i++; j++;}
+        else if(j == 0) i++;
+        else j = failure[j-1]+1;
     }
-    printf("\n");
-    for(int j = 0; j<n; j++){
-        printf("%3d", failure[j]);
-    }
+    return (j==lenp ? i-lenp : -1);
 }
 
 int main()
 {
-    char str[25]={"aabcddaabbaabcddaabcdda"};
-    fail(str);
+    char string[100]={"aabcddaab'jaejung'cddaabcddfaa'jaejung'asdfa"};
+    char pattern[100] = {"jaejung"};
+    createFailFunc(pattern);
+    int startp = patternMatch(string, pattern);
+
+    printf("start point about \'%s\': %d\n", pattern, startp);
+    for(int i=startp; i<startp+strlen(pattern); i++){
+        printf("%c", string[i]);
+    }
+    printf("\n");
     return 0;
 }
