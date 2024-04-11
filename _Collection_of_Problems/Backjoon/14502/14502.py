89c15a22bd9e755622301c03bc80e58b0f3b1e9e
Jaejung <ljj0512@gmail.com>
2024-04-11 20:30:11 +0900

    UPGRAD: 14502.py in python

--- _Collection_of_Problems/Backjoon/14502/14502.py
+++ _Collection_of_Problems/Backjoon/14502/14502.py
@@ -7,30 +7,33 @@ from itertools import combinations
 
 N, M = map(int, input().split(" "))
 building = [ list(map(int, input().split(" "))) for _ in range(N) ]
+result = 0
+zero_x_y = [(x, y) for x in range(M) for y in range(N) if not building[y][x]]
+first_virus = []
+for y in range(N):
+    for x in range(M):
+        if building[y][x] == 2:
+            first_virus.append((y, x))
+
 
 def cal_not_infected_areas(building: list[list[int]]) -> int:
-    global N, M
+    global N, M, first_virus
     dx = [1,-1,0,0]
     dy = [0,0,1,-1]
-    queue = deque()
-    for y in range(N):
-        for x in range(M):
-            if building[y][x] == 2:
-                queue.append((y,x))
+    queue = deque(first_virus)
 
     while len(queue):
         y, x = queue.popleft()
         for i in range(4):
-            next_x, next_y = (x+dx[i], y+dy[i])
-            if (0 <= next_y < N) and (0 <= next_x < M) and (building[next_y][next_x] == 0):
-                building[next_y][next_x] = 2
-                queue.append((next_y, next_x))
+            nx, ny = (x+dx[i], y+dy[i])
+            if (0 <= ny < N) and (0 <= nx < M) and (building[ny][nx] == 0):
+                building[ny][nx] = 2
+                queue.append((ny, nx))
     
     count = sum([i.count(0) for i in building])
     return count
 
-result = 0
-zero_x_y = [(x, y) for x in range(M) for y in range(N) if not building[y][x]]
+
 for c in combinations(zero_x_y, 3):
     temp_building = copy.deepcopy(building)
     for x, y in c:
